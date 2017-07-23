305c162dfbe5cf3b2cd2cf13e874408fc83bd9c4
Sattvik Chakravarthy <sattvik@gmail.com>
2017-07-23 23:06:03 +0530

    Re-organizing storage modules, using database module.

--- wing_session/module.py
+++ wing_session/module.py
@@ -1,3 +1,5 @@
+from drongo.utils import dict2
+
 import uuid
 
 
@@ -5,23 +7,30 @@ class Session(object):
     def __init__(self, app, **config):
         self.app = app
 
+        config = dict2.from_dict(config)
         self.cookie_name = config.get('cookie_name', '_drongo_sessid')
         self.session_var = config.get('session_var', 'session')
 
         # Load and configure the session storage
         storage = config.get('storage', 'filesystem')
         self.storage = None
+
         if storage == 'filesystem':
-            from .storage.filesystem import Filesystem
+            from .storage._filesystem import Filesystem
             path = config.get('session_path', './.sessions')
             self.storage = Filesystem(path=path)
+
         elif storage == 'mongo':
-            from .storage.mongo import Mongo
-            collection = config.get('collection')
-            self.storage = Mongo(collection=collection)
+            from .storage._mongo import Mongo
+            database_module = config.modules.database
+            collection = config.get('collection', 'session')
+            self.storage = Mongo(
+                collection=database_module.instance.get_collection(collection))
+
         elif storage == 'redis':
-            from .storage.redis import Redis
-            db = config.get('db')
+            from .storage._redis import Redis
+            database_module = config.modules.database
+            db = database_module.instance.get()
             self.storage = Redis(db=db)
 
         app.add_middleware(self)
